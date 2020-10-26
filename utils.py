1e267b75db5ac8b15aa1e3f5125bea4fad766cb3
Job van der Wal <48634934+J08K@users.noreply.github.com>
2020-10-26 08:35:16 +0100

    Updated

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
-import json, datetime, os
+import datetime
+import json
+import os
 
 ENVIRONMENT_DIR = "C:\\"
 
@@ -8,9 +10,26 @@ def check_last_session():
     return directory_info[len(directory_info) - 1]
 
 
-class MoodUtils:
+class MoodSession:
     session_id = int()
     session_start = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    """
+    Message structure
+    {
+        "Session_id":1,
+        "Session_start":date,
+        "Moods":
+        [
+            {
+            "time":time,
+            "message":message
+            "mood":mood
+            }
+        ]
+
+    }
+    """
 
     def __init__(self):
         os.chdir(ENVIRONMENT_DIR)
@@ -27,20 +46,40 @@ class MoodUtils:
             os.mkdir("printable")
             print("Created directory: printable")
 
+        print("")
+
         self.session_id = int(check_last_session()[:-5]) + 1
-        with open(str(self.session_id) + ".json", "w+") as file:
-            json.dump({"Session_id": self.session_id, "Session_start": self.session_start}, file, indent=2)
-        with open(str(self.session_id) + ".txt", "w+") as file:
-            file.write(f"Date: {self.session_start}\nSession number {self.session_id}\n"+"-"*20)
+        with open(str("json\\" + self.session_id) + ".json", "w+") as file:
+            json.dump({"Session_id": self.session_id, "Session_start": self.session_start, "Moods": []}, file, indent=2)
 
     def add_message(self, text):
-        pass
+        with open("json\\" + str(self.session_id) + ".json", "r") as file:
+            file_data = json.load(file)
+            file_data["Moods"].append(
+                {"time": datetime.datetime.now().strftime("%H:%M:%S"), "message": text, "mood": ""})
+        with open("json\\" + str(self.session_id) + ".json", "w") as file:
+            file.dump(file_data, file, indent=2)
 
     def add_mood(self, index, mood):
-        pass
+        with open("json\\" + str(self.session_id) + ".json", "r") as file:
+            file_data = json.load(file)
+            for mood_index in range(len(file_data["Moods"])):
+                if mood_index == index:
+                    file_data["Moods"][mood_index]["mood"] = mood
+        with open("json\\" + str(self.session_id) + ".json", "w") as file:
+            json.dump(file_data, file, indent=2)
 
     def list_messages(self, exclude=True):
-        pass
+        with open("json\\" + str(self.session_id) + ".json", "r") as file:
+            file_data = json.load(file)
+            temp_data = list()
+            for mood, index in zip(file_data["Moods"], range(len(file_data["Moods"]))):
+                if exclude:
+                    if mood["mood"] != "":
+                        temp_data.append(tuple(index, mood))
+                else:
+                    temp_data.append(tuple(index, mood))
+            return temp_data
 
 
-a = MoodUtils()
+a = MoodSession()
